5930c4974d4521c0d7e2852471248e8ca10521ec
mjq2020 <mjqx2011@163.com>
2025-12-02 08:10:13 +0000

    add: hot load

--- backend/app/__main__.py
+++ backend/app/__main__.py
@@ -3,4 +3,4 @@ from __future__ import annotations
 import uvicorn
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=False)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8000, reload=True)
